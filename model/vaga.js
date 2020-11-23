47acfa67db0cf3d18e8992b2dd02b246440d944f
cleissomfb <cleissom75@gmail.com>
2020-11-22 23:22:47 -0300

    Adicionado novas váriaveis ao schema de vagas.

--- model/vaga.js
+++ model/vaga.js
@@ -5,7 +5,17 @@ mongoose.Promise = global.Promise;
 
 const VagaSchema = new Schema({
     nome: String,
-    descricao: String,
+    atividade: String,
+    numeroVagas: String,
+    remuneracao: String,
+    possibilidadeEfetivacao: String,
+    horario: String,
+    cargaHoraria: String,
+    requisitos: String,
+    SemestreMinimo: String,
+    beneficios: String,
+    outrosBeneficios: String,
+    observacao: String,
     empresa: { type: Schema.ObjectId, ref: 'Empresa' },
     interessados: [{ type: Schema.ObjectId, ref: 'Usuario' }]
 }, {
